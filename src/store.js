3035b9afb6bb5ada74c11f555a7ab753effc232f
skmajumder <skmajumder.cse@gmail.com>
2023-10-30 22:34:43 +0600

    redux dispatch & getState

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -14,7 +16,12 @@ function reducer(state = initialState, action) {
 
     case "account/requestLoan":
       if (state.loan > 0) return;
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
 
     case "account/payLoan":
       return { ...state, loan: 0, loanPurpose: "" };
@@ -23,3 +30,16 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 2000 });
+store.dispatch({ type: "account/withdraw", payload: 500 });
+console.log(store.getState());
+
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 1000, purpose: "Business investment" },
+});
+
+console.log(store.getState());
